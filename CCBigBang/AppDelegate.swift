ee9ad388224db025f968f336260a9991934e825f
xiaosao6 <chens@pcbdoor.com>
2018-01-02 15:32:39 +0800

    优化结构

--- CCBigBang/AppDelegate.swift
+++ CCBigBang/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ViewDeck
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
