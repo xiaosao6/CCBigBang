ca74facbe4ab84f9e8f4d5e52071b57402300d25
xiaosao6 <chens@pcbdoor.com>
2018-01-04 16:45:50 +0800

    添加测试用Cell

--- CCBigBangExt/TodayViewController.swift
+++ CCBigBangExt/TodayViewController.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 import NotificationCenter
+import UICollectionViewLeftAlignedLayout
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+    
+    fileprivate lazy var collView : UICollectionView = {
+        let flowLayout = UICollectionViewLeftAlignedLayout.init()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumInteritemSpacing = 5; flowLayout.minimumLineSpacing = 5
+        let tmpcollView = UICollectionView.init(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        tmpcollView.showsVerticalScrollIndicator = false
+        tmpcollView.allowsMultipleSelection = true
+        tmpcollView.backgroundColor = UIColor.white
+        tmpcollView.delegate = self; tmpcollView.dataSource = self
+        tmpcollView.register(ExtWordCell.self, forCellWithReuseIdentifier: NSStringFromClass(ExtWordCell.self))
+        return tmpcollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.green
         
         if #available(iOSApplicationExtension 10.0, *) {
             self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
         } else {
             // Fallback on earlier versions
+        }
+        
+        self.view.addSubview(collView)
+        collView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview().inset(15)
         }
     }
     
@@ -35,6 +53,22 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @available(iOSApplicationExtension 10.0, *)
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
         preferredContentSize = maxSize
+    }
+    
+}
+
+extension TodayViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { return 50 }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: 70, height: 35)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NSStringFromClass(ExtWordCell.self), for: indexPath) as! ExtWordCell
+        cell.contentView.backgroundColor = UIColor.green
+        return cell
     }
     
 }
