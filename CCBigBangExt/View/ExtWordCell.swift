05e0c85178ed321d6cc02e048b0e8c10544b0163
xiaosao6 <chens@pcbdoor.com>
2018-01-04 16:33:17 +0800

    添加ExtWordCell

---
+++ CCBigBangExt/View/ExtWordCell.swift
@@ -0,0 +1,38 @@
+//
+//  ExtWordCell.swift
+//  CCBigBangExt
+//
+//  Created by sischen on 2018/1/4.
+//  Copyright © 2018年 pcbdoor.com. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ExtWordCell: UICollectionViewCell {
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel.init()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.layer.backgroundColor = UIColor.lightGray.cgColor
+        label.layer.masksToBounds = true
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview().inset(5)
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) }
+    
+    func configUI(text: String?) -> () {
+        titleLabel.text = text
+    }
+}
--- Podfile
+++ Podfile
@@ -16,3 +16,17 @@ target "CCBigBang" do
     pod 'ViewDeck'
 end
 
+target "CCBigBangExt" do
+    pod 'MJExtension'
+    pod 'AFNetworking',             '~> 3.1.0'
+    pod 'Kingfisher'                #(3.3.3)
+    pod 'SnapKit'
+    pod 'UICollectionViewLeftAlignedLayout'
+    #    pod 'RxSwift'
+    #    pod 'RxCocoa'
+    pod 'MMWormhole'
+    pod 'MBProgressHUD',            '~> 0.9.2'
+    pod 'Toast',                    '~> 3.1.0'
+    pod 'ViewDeck'
+end
+
